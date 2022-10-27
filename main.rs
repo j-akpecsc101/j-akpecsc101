06b123b0dbfe3ec55503779794f592c599af9dbf
j-akpecsc101 <115728897+j-akpecsc101@users.noreply.github.com>
2022-10-27 16:22:48 +0100

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let school_string = "school of science and technology";
-    let rating_float = 5.0;
-    let is_growing_boolean = true;
-    let department_integer = 3;
+    let result = 10;   // i32 by default
+    let age:u32 = 20;
+    let sum:i32 = 5-15;
 
-
-    println!("school name is: {}",school_string);
-    println!("school rating out of 5 is: {}",rating_float);
-    println!("school is growing: {}",is_growing_boolean);
-    println!("Number of departments: {}",department_integer);  
+    println!("Result value is {}",result);
+    println!("Age is {}",age);
+    println!("Sum is {}",sum);
 }
