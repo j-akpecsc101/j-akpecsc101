81b68c7be4e68c11de7e85826267bc4f3eb82577
j-akpecsc101 <115728897+j-akpecsc101@users.noreply.github.com>
2022-10-27 16:22:11 +0100

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let school_string = "school of science and technology";
+    let rating_float = 5.0;
+    let is_growing_boolean = true;
+    let department_integer = 3;
+
+
+    println!("school name is: {}",school_string);
+    println!("school rating out of 5 is: {}",rating_float);
+    println!("school is growing: {}",is_growing_boolean);
+    println!("Number of departments: {}",department_integer);  
+}
