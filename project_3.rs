312daa127b19bb6fa11f8ee6d53d3dc6e1b2f17f
j-akpecsc101 <115728897+j-akpecsc101@users.noreply.github.com>
2022-10-28 15:06:55 +0100

    Delete project_3.rs

--- project_3.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-	let tv:f64 = 210_000.0;
-	let dep:f64 = 5.0/100.0;
-	let yr1p = tv - dep*tv;
-	let yr2p = yr1p - dep*yr1p;
-	let yr3p = yr2p - dep*yr2p;
-	println!("Value after 3 years = {}", yr3p );
-}
